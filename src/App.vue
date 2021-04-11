512c6716c6590489e8a39eacc0b2b1b070f7435a
bobasyu <bobasyu@163.com>
2021-04-11 15:20:34 +0800

    温度展示

--- src/App.vue
+++ src/App.vue
@@ -6,11 +6,11 @@
           <template #title><i class="el-icon-s-data"></i>数据展示</template>
           <el-menu-item-group>
             <template #title>温湿度</template>
-            <el-menu-item index="1-1" @click="routerTo('data_dialogue')">图表展示</el-menu-item>
-            <el-menu-item index="1-2" @click="routerTo('data_detail')">详情查看</el-menu-item>
+            <!--            <el-menu-item index="1-1" @click="routerTo('data_dialogue')">图表展示</el-menu-item>-->
+            <el-menu-item index="1-2" @click="routerTo('data_detail', '数据详情展示')">详情查看</el-menu-item>
           </el-menu-item-group>
           <el-menu-item-group title="摄像头">
-            <el-menu-item index="1-3" @click="routerTo('camera')">查看摄像头</el-menu-item>
+            <el-menu-item index="1-3" @click="routerTo('camera', '摄像头展示')">查看摄像头</el-menu-item>
           </el-menu-item-group>
         </el-submenu>
 
@@ -18,19 +18,20 @@
           <template #title><i class="el-icon-setting"></i>指令操作</template>
           <el-menu-item-group>
             <template #title>手动指令</template>
-            <el-menu-item index="2-1" @click="routerTo('send_instruction')">执行操作指令</el-menu-item>
-            <el-menu-item index="2-2" @click="routerTo('instruction_detail')">操作指令记录</el-menu-item>
+            <el-menu-item index="2-1" @click="routerTo('send_instruction', '执行操作指令')">执行操作指令</el-menu-item>
+            <el-menu-item index="2-2" @click="routerTo('instruction_detail', '操作指令记录')">操作指令记录</el-menu-item>
           </el-menu-item-group>
           <el-menu-item-group title="定时指令">
-            <el-menu-item index="2-3" @click="routerTo('create_timing_task')">设定定时任务</el-menu-item>
-            <el-menu-item index="2-4" @click="routerTo('timing_task_detail')">定时任务记录</el-menu-item>
+            <el-menu-item index="2-3" @click="routerTo('create_timing_task','设定定时任务')">设定定时任务</el-menu-item>
+            <el-menu-item index="2-4" @click="routerTo('timing_task_detail','定时任务记录')">定时任务记录</el-menu-item>
           </el-menu-item-group>
         </el-submenu>
       </el-menu>
     </el-aside>
 
     <el-container>
-      <el-header style="text-align: right; font-size: 12px">
+      <el-header style="text-align: left; font-size: 20px">
+        {{ msg }}
       </el-header>
 
       <el-main>
@@ -43,8 +44,14 @@
 <script>
 export default {
   name: "App",
+  data() {
+    return {
+      msg: '主页',
+    }
+  },
   methods: {
-    routerTo: function (routerPath) {
+    routerTo: function (routerPath, msg) {
+      this.msg = msg
       this.$router.push({
         path: routerPath
       })
--- src/views/data/dataDetail.vue
+++ src/views/data/dataDetail.vue
@@ -1,14 +1,101 @@
 <template>
-  <h1>{{ msg }}</h1>
+  <div style="width: 800px;height:700px;float: left">
+    <h1 style="color:red;font-size: 30px">当前温度: {{ currentTemperature }}℃</h1>
+    <br>
+    <div id="main" style="width: 800px;height:600px;float: left"></div>
+  </div>
+
+  <el-table :data="tableData" border style="width: 600px;height: 700px" :row-class-name="tableRowClassName"
+            height="250">
+    <el-table-column fixed prop="id" label="编号" width="120px"></el-table-column>
+    <el-table-column fixed prop="temperature" label="温度" width="120px"></el-table-column>
+    <el-table-column fixed prop="createTime" label="创建时间" width="220px"></el-table-column>
+    <el-table-column fixed="right" label="操作" width="100">
+      <template #default="scope">
+        <el-button type="text" size="small" @click="deleteTemperature(scope.row)">删除</el-button>
+      </template>
+    </el-table-column>
+  </el-table>
 </template>
 
 <script>
+import axios from "_axios@0.21.1@axios";
+import echarts from "_echarts@4.9.0@echarts";
+
 export default {
-  name: "dataDetail",
-  data() {
+  name: "dataDetail", data() {
     return {
-      msg: '数据详情展示'
+      msg: "数据详情展示",
+      currentTemperature: 0,
+      tableData: [
+        {
+          id: '',
+          temperature: '',
+          createTime: '',
+        },
+      ]
     }
+  },
+  methods: {
+    // eslint-disable-next-line no-unused-vars
+    tableRowClassName({row, rowIndex}) {
+      if (rowIndex === 1) {
+        return 'warning-row';
+      } else if (rowIndex === 3) {
+        return 'success-row';
+      }
+      return '';
+    },
+    getTemperature: function () {
+      const _this = this;
+      axios.get("http://127.0.0.1:8000/api/temperature").then(function (response) {
+        _this.tableData = response.data
+
+        let times = Array()
+        let temperatures = Array()
+        for (let i in response.data) {
+          times.push(response.data[i].createTime)
+          temperatures.push(response.data[i].temperature)
+        }
+        _this.currentTemperature = temperatures[response.data.length - 1]
+        let myChart = echarts.init(document.getElementById('main'));
+        let option = {
+          title: {
+            text: '温度变化曲线',
+          },
+          tooltip: {},
+          legend: {
+            data: ['温度']
+          },
+          xAxis: {
+            data: times
+          },
+          yAxis: {},
+          series: [{
+            name: '温度',
+            type: 'line',
+            data: temperatures
+          }]
+        };
+        myChart.setOption(option);
+      })
+    },
+    deleteTemperature: function (row) {
+      const _this = this
+      // eslint-disable-next-line no-unused-vars
+      axios.delete("http://127.0.0.1:8000/api/temperature/" + row.id).then(function (response) {
+        _this.alert(row.cname + '删除成功', 'success', {
+          // eslint-disable-next-line no-unused-vars
+          callback: action => {
+            window.local.reload();
+            _this.getTemperature();
+          }
+        })
+      })
+    },
+  },
+  created() {
+    this.getTemperature()
   }
 }
 </script>
